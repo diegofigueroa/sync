cc6fe3902c81ee4710512aff8baa1e3edee9c246
Diego Figueroa <diegofigueroa99@gmail.com>
2013-04-16 19:17:21 -0600

    removed duplicated application.css, removed home_controller.rb (why was this here?), fixed routes.rb so :root points to dashboard#index. removed both index.html files.

--- config/routes.rb
+++ config/routes.rb
@@ -56,5 +56,5 @@ Nospi::Application.routes.draw do
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
   resources :dashboard
-  root to: "home#index"
+  root to: "dashboard#index"
 end

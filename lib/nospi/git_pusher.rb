3d10d7a25b49e60bf20fb428e48e0e790a8841a6
Ed Mckain <mckain.default@gmail.com>
2013-04-18 22:24:24 -0600

    improves GitPusher.pull method.

--- lib/nospi/git_pusher.rb
+++ lib/nospi/git_pusher.rb
@@ -10,19 +10,27 @@ require 'grit'
 class GitPusher
 
   def self.addRemote remoteName, repoToPush
-  	repo.git.remote({},'add',remoteName,repoToPush)
+    tmpPath = '/tmp/'+remoteName+"/"     #Actualizar Path, al Rails PAth tmp
+    repo = Grit::Git.new(tmpPath)
+    #repo.git.remote({},'rename','origin','upstream')    # This rename will let us upload the code.
+  	repo.remote({},'add',remoteName,repoToPush)
   end
 
 
-  def self.push project_id, branch
+  def self.push project_id, branch = 'master'
+    require 'grit'
     project = Project.find project_id
-    url = project.source_code_url
-    tmpPath = '/tmp/'    #Actualizar Path, al Rails PAth tmp
+    url = project.nosi_github_repo_name
+    tmpPath = '/tmp/'+project.friendly_name+"/"     #Actualizar Path, al Rails PAth tmp
 
   	repo = Grit::Git.new(tmpPath)
     remote = repo.remotes.last
-    self.addRemote project.friendly_name, url
-	  pusher = repo.git.push({:process_info => true, :progress => true}, project.friendly_name, branch)
+    #
+    #self.addRemote project.friendly_name, url
+    #sets the remote repo into the project called 'MyRemoteRepo'
+    repo.git.remote({},'add',project.friendly_name,url)
+    #
+    pusher = repo.git.push({:process_info => true, :progress => true}, project.friendly_name, branch)
   end
 
 end

6f85163e1f22d76dcfe56cdcbba248d68dc517a2
Diego Figeroa <diegofigeroa99@gmail.com>
2013-09-21 11:34:54 -0600

    add new seeds for lincenses data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 GithubSettings.create username: "nasasync", client_id: "9d290ef49d7aa71409d2", client_secret: "8dad57de0fa3fcb8e4775d25541e7ded62b70a5d"
+License.create title: 'NASA Open Source Agreement'
+License.create title: 'Apache Version 2.0'
+License.create title: 'BSD'
+License.create title: 'GPL 3'
